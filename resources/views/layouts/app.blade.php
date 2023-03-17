6e187fe7ca15c6ea39d6c11aae1ad35efa48d9a7
Christian Morales <chrisrodo84@gmail.com>
2023-03-17 11:11:01 -0600

    Fixing some errors

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,9 +6,8 @@
 
 		<title>Vexatragram - @yield('titulo')</title>
 		@stack('styles')
-		<link rel="stylesheet" href="{{ asset('css/app.css') }}">
-		{{-- @vite('resources/css/app.css')
-		@vite('resources/js/app.js') --}}
+		@vite('resources/css/app.css')
+		@vite('resources/js/app.js')
 		@livewireStyles
 	</head>
 
